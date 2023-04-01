ca2ac9c0876de3a4b8782176ff825fb9a979a571
Tamino Dauth <taminojdauth@gmail.com>
2023-04-01 16:44:42 +0200

    Update data and move images

---
+++ updatemapdata.bat
@@ -0,0 +1,4 @@
+copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-Items.txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
+copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-Creeps.txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
+copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-Heroes.txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
+pause
