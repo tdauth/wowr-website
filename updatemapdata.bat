fb2d23507a546d92be79eab8494140a4df305d18
Tamino Dauth <taminojdauth@gmail.com>
2023-04-01 20:05:42 +0200

    races and items

--- updatemapdata.bat
+++ updatemapdata.bat
@@ -2,4 +2,5 @@ copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforg
 copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-Creeps.txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
 copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-Heroes.txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
 copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-Players.txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
+copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-Races.txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
 pause
