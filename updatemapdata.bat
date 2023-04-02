5dc1cbb1d02c30c49fa47e3560a9abed16437649
Tamino Dauth <taminojdauth@gmail.com>
2023-04-02 05:30:06 +0200

    Bosses and icons

--- updatemapdata.bat
+++ updatemapdata.bat
@@ -5,4 +5,5 @@ copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforg
 copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-Races.txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
 copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-Quests.txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
 copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-Researches.txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
+copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-Bosses.txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
 pause
