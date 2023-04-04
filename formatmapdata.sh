5eb41cea1c6a2bf9d7b405aefd29c913274138a4
Tamino Dauth <taminojdauth@gmail.com>
2023-04-04 08:40:06 +0200

    Icons and script

---
+++ formatmapdata.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Replace the following sections from the txt file:
+
+```
+" )
+	call Preload( "
+```
+
+```
+	" )
+	call PreloadEnd( 10.2 )
+
+endfunction
+```
+
+```
+function PreloadFiles takes nothing returns nothing
+
+	call PreloadStart()
+```
+
+# Remove the content between two tbody elements in the given file.
+
+# Insert the content after tbody in the given file.
