6b2f4d0636b5b8fc769f081de8d0b0f0afa7b7dd
Tamino Dauth <taminojdauth@gmail.com>
2023-04-18 07:58:30 +0200

    Data

--- updatemapdata.bat
+++ updatemapdata.bat
@@ -14,4 +14,5 @@ copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforg
 copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-Mounts.txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
 copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-ChatCommands.txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
 copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-HeroJourney.txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
+copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-Locations.txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
 pause
