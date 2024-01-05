16ccea065614bd1fc623fb1cb30899ba70a46798
Tamino Dauth <taminojdauth@gmail.com>
2024-01-05 22:41:34 +0100

    data

--- updatemapdata.bat
+++ updatemapdata.bat
@@ -21,4 +21,5 @@ copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforg
 copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-Achievements.txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
 copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-HeroStandardAbilities.txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
 copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-Race-"*".txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
+copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\WorldOfWarcraftReforged-ChangeLog-"*".txt" "C:\Users\Tamino\Documents\Projekte\wowr-website\map"
 pause
